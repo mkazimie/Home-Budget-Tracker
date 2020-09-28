c03f9cfb7aabbb6028b057b5b20221e294bac145
Magdalena Kazimieruk <mkazimieruk@hotmail.com>
2020-09-28 16:54:56 +0200

    App setup

---
+++ src/main/java/com/example/budgetary/BudgetaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.budgetary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/budgetary/BudgetaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budgetary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
